8e002f8e9520c8e881bda140a0a7bb4418051f3a
kengledo <45169493+kengledo@users.noreply.github.com>
2020-03-11 13:54:59 +0000

    Add files via upload

---
+++ Firepower_O365_Feed_Parser-VERSION-3.0/O365WebServiceParser.py
@@ -0,0 +1,387 @@
+# NOTE: this is a Proof of Concept script, please test before using in production!
+
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# This software is licensed to you under the terms of the Cisco Sample
+# Code License, Version 1.0 (the "License"). You may obtain a copy of the
+# License at
+#                https://developer.cisco.com/docs/licenses
+# All use of the material herein must be in accordance with the terms of
+# the License. All rights not expressly granted by the License are
+# reserved. Unless required by applicable law or agreed to separately in
+# writing, software distributed under the License is distributed on an "AS
+# IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.
+
+import getpass
+import json
+import os
+import requests
+import sys
+import datetime
+import time
+import uuid
+import ciscosparkapi
+# import supporting functions from additional file
+from Firepower import Firepower
+
+# Config Paramters
+CONFIG_FILE     = "config.json"
+CONFIG_DATA     = None
+
+# Object Prefix
+OBJECT_PREFIX = ""
+
+# A function to load CONFIG_DATA from file
+def loadConfig():
+
+    global CONFIG_DATA
+
+    sys.stdout.write("\n")
+    sys.stdout.write("Loading config data...")
+    sys.stdout.write("\n")
+
+    # If we have a stored config file, then use it, otherwise create an empty one
+    if os.path.isfile(CONFIG_FILE):
+
+        # Open the CONFIG_FILE and load it
+        with open(CONFIG_FILE, 'r') as config_file:
+            CONFIG_DATA = json.loads(config_file.read())
+
+        sys.stdout.write("Config loading complete.")
+        sys.stdout.write("\n")
+        sys.stdout.write("\n")
+
+    else:
+
+        sys.stdout.write("Config file not found, loading empty defaults...")
+        sys.stdout.write("\n")
+        sys.stdout.write("\n")
+
+        # Set the CONFIG_DATA defaults
+        CONFIG_DATA = {
+            "FMC_IP": "",
+            "FMC_USER": "",
+            "FMC_PASS": "",
+            "IP_UUID": "",
+            "URL_UUID": "",
+            "SERVICE":  False,
+            "SSL_VERIFY": False,
+            "SSL_CERT": "/path/to/certificate",
+            "AUTO_DEPLOY": False,
+            "VERSION":  0,
+            "WEBEX_ACCESS_TOKEN": "",
+            "WEBEX_ROOM_ID": "",
+        }
+
+# A function to store CONFIG_DATA to file
+def saveConfig():
+
+    sys.stdout.write("Saving config data...")
+    sys.stdout.write("\n")
+
+    with open(CONFIG_FILE, 'w') as output_file:
+        json.dump(CONFIG_DATA, output_file, indent=4)
+
+# A function to deploy pending policy pushes
+def DeployPolicies(fmc):
+
+    # Get pending deployments
+    pending_deployments = fmc.getPendingDeployments()
+
+    # Setup a dict to hold our deployments
+    deployments = {}
+
+    # See if there are pending deployments
+    if pending_deployments['paging']['count'] > 0:
+
+        # Iterate through pending deployments
+        for item in pending_deployments['items']:
+
+            # Only get ones that can be deployed
+            if item['canBeDeployed']:
+
+                # Only get ones that don't cause traffic interruption
+                if item['trafficInterruption'] == "NO":
+
+                    # If there are multiple devices, append them
+                    if item['version'] in deployments:
+                        device_list = deployments[item['version']]
+                        device_list.append(item['device']['id'])
+                        deployments[item['version']] = device_list
+                    else:
+                        deployments[item['version']] = [item['device']['id']]
+
+        # Build JSON for each of our deployments
+        for version, devices in deployments.items():
+
+            deployment_json = {
+                "type": "DeploymentRequest",
+                "version": version,
+                "forceDeploy": False,
+                "ignoreWarning": True,
+                "deviceList": devices,
+            }
+
+            fmc.postDeployments(deployment_json)
+
+        sys.stdout.write("All pending deployments have been requested.\n")
+    
+    else:
+
+        sys.stdout.write("There were zero pending deployments.\n")
+
+# Function that can be used to schedule the O365WebServiceParser to refresh at intervals. Caution: this creates an infinite loop.
+# Takes the O365WebServiceParser function and the interval as parameters. 
+def intervalScheduler(function, interval):
+
+    # user feedback
+    sys.stdout.write("\n")
+    sys.stdout.write("O365 Web Service Parser will be refreshed every %d seconds. Please use ctrl-C to exit.\n" %interval)
+    sys.stdout.write("\n")
+
+    # interval loop, unless keyboard interrupt
+    try:
+        while True:
+            function()
+            # get current time, for user feedback
+            date_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            sys.stdout.write("\n")
+            sys.stdout.write("[%s] O365 Web Service Parser executed by IntervalScheduler, current interval is %d seconds. Please use ctrl-C to exit.\n" % (date_time, interval))
+            sys.stdout.write("\n")
+            # sleep for X amount of seconds and then run again. Caution: this creates an infinite loop to check the Web Service Feed for changes
+            time.sleep(interval)
+
+    # handle keyboard interrupt
+    except (KeyboardInterrupt, SystemExit):
+        sys.stdout.write("\n")
+        sys.stdout.write("\n")
+        sys.stdout.write("Exiting... O365 Web Service Parser will not be automatically refreshed anymore.\n")
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        pass
+
+
+# function to parse the Web Service, so that it can be called iteratively (e.g by the scheduler function)
+def WebServiceParser():
+
+    # Instantiate a Firepower object
+    fmc = Firepower(CONFIG_DATA)
+
+    # If there's no defined Network Object, make one, then store the UUID - else, get the current object
+    if CONFIG_DATA['IP_UUID'] is '':
+
+        # Create the JSON to submit
+        object_json = {
+            'name': OBJECT_PREFIX + 'O365_Web_Service_IPs',
+            'type': 'NetworkGroup',
+            'overridable': True,
+        }
+
+        # Create the Network Group object in the FMC
+        ip_group_object = fmc.createObject('networkgroups', object_json)
+
+        # Save the UUID of the object
+        CONFIG_DATA['IP_UUID'] = ip_group_object['id']
+        saveConfig()
+    else:
+        # Get the Network Group object of the specified UUID
+        ip_group_object = fmc.getObject('networkgroups', CONFIG_DATA['IP_UUID'])
+
+    # If there's no defined URL Object, make one, then store the UUID
+    if CONFIG_DATA['URL_UUID'] is '':
+
+        # Create the JSON to submit
+        object_json = {
+            'name': OBJECT_PREFIX + 'O365_Web_Service_URLs',
+            'type': 'UrlGroup',
+            'overridable': True,
+        }
+
+        # Create the URL Group object in the FMC
+        url_group_object = fmc.createObject('urlgroups', object_json)
+
+        # Save the UUID of the object
+        CONFIG_DATA['URL_UUID'] = url_group_object['id']
+        saveConfig()
+    else:
+        # Get the URL Group object of the specified UUID
+        url_group_object = fmc.getObject('urlgroups', CONFIG_DATA['URL_UUID'])
+
+    # Get the latest version of the loaded feed
+    latestVersion = CONFIG_DATA['VERSION']
+
+    # create GUID for GET requests
+    clientRequestId = str(uuid.uuid4())
+
+    # URL needed to check latest version
+    webServiceVersionURL = "https://endpoints.office.com/version?clientrequestid="
+
+    # assemble URL for get request for version 
+    getURLVersion = webServiceVersionURL + clientRequestId
+
+    # do GET request 
+    reqVersion = requests.get(getURLVersion)
+
+    # grab output in JSON format
+    version = reqVersion.json()
+
+    # loop through version list and grab Wordwide list version
+    for element in version:
+        if(element['instance'] == 'Worldwide'):
+            newVersion = int(element['latest'])
+
+    # if the version did not change, the Web Service feed was not updated. 
+    if(newVersion == latestVersion):
+
+        # user feed back
+        sys.stdout.write("\n")
+        sys.stdout.write("Web Service List has NOT been updated since the last load, no update needed!\n") 
+        sys.stdout.write("\n")
+
+    # check if there is a newer version 
+    if(newVersion > latestVersion):
+
+        # update version and save the config
+        CONFIG_DATA['VERSION'] = newVersion
+
+        # user feedback
+        sys.stdout.write("\n")
+        sys.stdout.write("New version of Office 365 worldwide commercial service instance endpoints detected: %(version)s" % {'version': CONFIG_DATA['VERSION']})
+        sys.stdout.write("\n")
+
+        ### PARSE JSON FEED ###
+
+        # URL needed for the worldwide web service feed
+        webServiceURL = "https://endpoints.office.com/endpoints/worldwide?clientrequestid="
+
+        # assemble URL for get request
+        getURL = webServiceURL + clientRequestId
+
+        # do GET request 
+        req = requests.get(getURL)  
+
+        # initiate lists to be filled with addresses
+        URL_List = []
+        IP_List = []
+
+        # error handling if true then the request was HTTP 200, so successful 
+        if(req.status_code == 200):
+
+            # grab output in JSON format
+            output = req.json()
+
+            # iterate through each 'item' in the JSON data
+            for item in output:
+
+                # make sure URLs exist in the item
+                if 'urls' in item:
+                    
+                    # iterate through all URLs in each item
+                    for url in item['urls']:
+
+                        # remove asterisks to put URLs into Firepower format 
+                        #   (https://www.cisco.com/c/en/us/support/docs/security/firesight-management-center/118852-technote-firesight-00.html#anc14)
+                        url = url.replace('*','')
+
+                        # if the URL hasn't already been appended, then append it
+                        if url not in URL_List:
+                            URL_List.append(url)
+
+                # make sure IPs exist in the item
+                if 'ips' in item:
+
+                    # iterate through all IPs in each item
+                    for ip in item['ips']:
+
+                        # if the IP hasn't already been appended, then append it
+                        if ip not in IP_List:
+                            IP_List.append(ip)
+
+        # Reset the fetched Network Group object to clear the 'literals'
+        ip_group_object['literals'] = []
+        ip_group_object.pop('links', None)
+
+        # Add all the fetched IPs to the 'literals'of the Network Group object
+        for ip_address in IP_List:
+            ip_group_object['literals'].append({'type': 'Network', 'value': ip_address})
+
+        # Update the NetworkGroup object
+        fmc.updateObject('networkgroups', CONFIG_DATA['IP_UUID'], ip_group_object)
+
+        # Reset the fetched URL Group object to clear the 'literals'
+        url_group_object['literals'] = []
+        url_group_object.pop('links', None)
+
+        # Add all the fetched URLs to the 'literals' of the URL Group object
+        for url in URL_List:
+            url_group_object['literals'].append({'type': 'Url', 'url': url})
+
+        # Update the UrlGroup object
+        fmc.updateObject('urlgroups', CONFIG_DATA['URL_UUID'], url_group_object)
+
+        # user feed back
+        sys.stdout.write("\n")
+        sys.stdout.write("Web Service List has been successfully updated!\n")
+        sys.stdout.write("\n")
+
+        saveConfig()
+
+        # If the user wants us to deploy policies, then do it
+        if CONFIG_DATA['AUTO_DEPLOY']:
+            DeployPolicies(fmc)
+        
+        # if Webex Teams tokens set, then send message to Webex room
+        if CONFIG_DATA['WEBEX_ACCESS_TOKEN'] is '' or CONFIG_DATA['WEBEX_ROOM_ID'] is '':
+
+            # user feed back
+            sys.stdout.write("Webex Teams not set.\n")
+            sys.stdout.write("\n")
+        else:
+
+            # adjust the Webex message based on the config
+            if CONFIG_DATA['AUTO_DEPLOY']:
+                message_text = "Microsoft Office 365 objects have been successfully updated!  Firepower policy deployment was initiated..."
+            else:
+                message_text = "Microsoft Office 365 objects have been successfully updated!  Firepower policy deployment is required."
+
+            # instantiate the Webex handler with the access token
+            webex = ciscosparkapi.CiscoSparkAPI(CONFIG_DATA['WEBEX_ACCESS_TOKEN'])
+
+            # post a message to the specified Webex room
+            message = webex.messages.create(CONFIG_DATA['WEBEX_ROOM_ID'], text=message_text)
+
+##############END PARSE FUNCTION##############START EXECUTION SCRIPT##############
+
+if __name__ == "__main__":
+
+    # Load config data from file
+    loadConfig()
+
+    # If not hard coded, get the FMC IP, Username, and Password
+    if CONFIG_DATA['FMC_IP'] is '':
+        CONFIG_DATA['FMC_IP'] = input("FMC IP Address: ")
+    if CONFIG_DATA['FMC_USER'] is '':
+        CONFIG_DATA['FMC_USER'] = input("FMC Username: ")
+    if CONFIG_DATA['FMC_PASS'] is '':
+        CONFIG_DATA['FMC_PASS'] = getpass.getpass("FMC Password: ")
+
+    # Save the FMC data
+    saveConfig()
+
+    try:
+        if CONFIG_DATA['SERVICE']:
+            # Calls the intervalScheduler for automatic refreshing (pass O365WebServiceParser function and interval in seconds (1 hour = 3600 seconds))
+            intervalScheduler(WebServiceParser, 3600) #set to 1 hour
+        else:
+            # Execute O365WebServiceParser just once
+            WebServiceParser()
+
+    except (KeyboardInterrupt, SystemExit):
+        sys.stdout.write("\n")
+        sys.stdout.write("\n")
+        sys.stdout.write("Exiting...\n")
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        pass
+
+# end of script
---
+++ Firepower_O365_Feed_Parser-VERSION-3.0/README.md
@@ -0,0 +1,130 @@
+[![published](https://static.production.devnetcloud.com/codeexchange/assets/images/devnet-published.svg)](https://developer.cisco.com/codeexchange/github/repo/chrivand/Firepower_O365_Feed_Parser)
+
+# O365 Web Service API to Firepower Objects Parser 
+
+This is a sample script that parses the [NEW O365 Web Service API](https://docs.microsoft.com/en-us/office365/enterprise/managing-office-365-endpoints#webservice) that Microsoft publishes with URL, IPv4 and IPv6 addresses. These addresses are used for the infrastructure of the Microsoft cloud applications (e.g., Office 365). The script will parse the NEW O365 Web Service API into 2 separate lists and use the FMC API to upload them into 2 Group Objects. These Group Objects can be used in a Firepower trust/prefilter rule. By doing so the traffic is excluded from further inspection, to prevent latency issues with the Microsoft O365 applications. 
+
+Please contact me, Christopher Van Der Made <chrivand@cisco.com>, if you have any questions or remarks. If you find any bugs, please report them to me, and I will correct them (or do a pull request).
+
+## Features
+
+* Retrieving Wordlwide URLs and IPs from new O365 REST-based web service; 
+* Parsing these into 2 flat lists (URL and IP (mixed IPv4 and IPv6));
+* Creating right JSON format for FMC API PUT requests;
+* Uploading this JSON to FMC, overwriting the previous Group Object;
+* If no objects have been created, 2 overridable objects will be created: 'O365_Web_Service_IPs' and 'O365_Web_Service_URLs';
+* Checking if O365 file was updated, using the O365 Version API Endpoint;
+* Automatic policy deploy using API when changes were made to Objects (optional, caution this will also deploy other, unrelated policy changes);
+* Webex Teams alert when changes were made to Objects;
+* Continuously checking for updates with a specified time interval (optional).
+
+### Potential next steps
+
+* Create extra modules for other SaaS applications;
+* Create extra modules for other Cisco solutions (WSA, Umbrella, etc.).
+
+
+## Solution Components
+
+The script consists of 3 python files. The main script can run indefinitely, leveraging a function that is built in, to rerun the script every x amount of seconds (it can also just be executed once). Then, using the Version API Endpoint, the script checks if changes were made to the Web Service list. If changes were made, the Web Service list is parsed and uploaded using a PUT request to FMC. Microsoft updates the Office 365 IP address and FQDN entries at the end of each month and occasionally out of cycle for operational or support requirements. Therefore, Microsoft recommends you check the version daily, or at the most, hourly. This can be automated with the script.
+
+### Cisco Products / Services
+
+* Cisco Firepower Management Center;
+* Cisco Firepower Threat Defense NGFW.
+
+
+## Installation
+
+These instructions will enable you to download the script and run it, so that the output can be used in Firepower as Group Objects. What do you need to get started? Please find a list of tasks below:
+
+1. You need the IP address (or domain name) of the FMC, the username and password. These will be requested by the script the first time it is run. It is recommended to create a separate FMC login account for API usage, otherwise the admin will be logged out during every API calls. Add the IP/Domain of FMC, the username and password to the config.json file. If you do not add anything, you will be promted to fill this in when executing the script.
+
+2. In the FMC, go to System > Configuration > REST API Preferences to make sure that the REST API is enabled on the FMC.
+
+3. A Network Group object and a URL Group object will be created automatically during the first run of the script. However, if you'd rather create the objects manually, you can follow the instructions below.
+
+4. It is also recommended to download an SSL certificate from FMC and put it in the same folder as the scripts. This will be used to securely connect to FMC. In the **config.json file**, set the *"SSL_VERIFY"* parameter to *true*, and then set *"SSL_CERT"* to be the path to the FMC's certificate.
+
+5. It is possible to integrate the script with Webex Teams. In order to do that, an API Access Token and a Room ID needs to be entered in the **config.json** file. Please retrieve your key from: https://developer.webex.com/docs/api/getting-started. Then create a dedicated Webex Teams space for these notifications and retrieve the Room ID from: https://developer.webex.com/docs/api/v1/rooms/list-rooms. 
+
+6. In this same Webex Teams room you can subscribe to an RSS feed from Microsoft regarding updates. Use this bot to integrate the RSS feed into your Webex space: https://apphub.webex.com/bots/rss-2739. The RSS feed URL can be found on the Microsoft website (link on top of this page).
+
+7. If you do not have the needed Python libraries set up, you will get an error when executing the script. You will need to install the *"requirements.txt"* file like this (make sure you are in the same directory as the cloned files live):
+
+```
+pip install -r requirements.txt
+```
+
+8. After this is complete you need to execute the script (make sure you are in the same directory as the cloned files live):
+
+```
+python3.6 O365WebServiceParser.py
+```
+
+9. Optionally you can let this script run periodically, by setting *"SERVICE"* to *true* in the **config.json** file. In line 374 of the **O365WebServiceParser.py** the time-period is set, per default it is set to an hour (Microsoft recommends you check the version daily, or at the most, hourly):
+
+```
+intervalScheduler(WebServiceParser, 3600) #set to 1 hour
+```
+
+10. Finally, if you want to automatically deploy the policies, you can set *"AUTO_DEPLOY"* to *true* in the **config.json** file. **Be very careful with this, as unfinished policies might be deployed by doing so.**
+
+### Manual Object Creation (Optional)
+
+1. Create 2 Group Objects in FMC: "O365_Web_Service_URLs" (URL Group Object) and "O365_Web_Service_IPs" (Network Group Object). At first you will have to put in a random URL/Network to create the group objects. No worries, we will override this later.
+
+2. Use the FMC API Explorer to do a GET request for the Network Group Objects. This is done by going into the FMC API Explorer (can be reached at https://IP-addressOfFMC/api/api-explorer), and then clicking on *"Object"* in the left menu. The scroll down to *"networkgroups"* and click on *"GET"* and then again on *"GET"* in the right menu. 
+
+3. Now you will need to copy-paste the Object IDs of the Network Group Object (*"O365_Web_Service_IPs"*). The IDs will look like the following format: *"000XXXX-YYYY-ZZZZ-0000-01234567890"*. This is displayed in the *"Response Text"* output box in the right menu. You will need these later in the PUT requests to update the objects. Below is an example of how this output would look for the *"O365_Web_Service_IPs"* Network Group Object:
+
+```
+"type": "NetworkGroup",
+"name": "O365_Web_Service_IPs",
+"id": "000XXXX-YYYY-ZZZZ-0000-01234567890"
+```
+
+4. Repeat the GET request of step 2 as well for *"urlgroups"*, to obtain the ID for the URL Group Object (*"O365_Web_Service_URLs"*). You should now have two IDs copy-pasted, which you can put inside the *config.json* file as *"IP_UUID"* and *"URL_UUID"* to configure the script.
+
+### How to use the Group Objects in Firepower Management Center.
+
+For better understanding of the packet flow in Firepower Threat Defense, and how the Fastpath action in the Prefilter Policy works, please review the following flow diagram:    
+
+![Networkobjects](screenshots_FMC_O365/packetflowftd.png)
+
+After the successful PUT requests, the 2 Group Objects will have been updated with the new IP-addresses and URLs. Please find screenshots of the 2 Group Objects, after the API call:
+
+![Networkobjects](screenshots_FMC_O365/screenshot_urlobject_new.png)
+
+![Networkobjects](screenshots_FMC_O365/screenshot_networkobject_new.png)
+
+These objects can be used in either Prefilter Policy Fastpath-rule (for the Network Object), or in an Access Control Policy Trust-rule (for the URL Object). This is an example of how to configure the Prefilter Policy rule in FMC:
+
+![Networkobjects](screenshots_FMC_O365/addprefilterrule.png)
+
+This will result in the following rule:
+
+![Networkobjects](screenshots_FMC_O365/fastpathrule.png)
+
+Likewise, this can be done with a Trust Rule in the Access Control Policy for the URL Group Object:
+
+![Networkobjects](screenshots_FMC_O365/trustrule.png)
+
+As a final step you will need to do a Policy Deploy, each time that the Group Objects have been updated. This can be done from the FMC by clicking on *"DEPLOY"* and by selecting the device that need this Policy Deploy.
+
+### Please take caution on the following notes:
+
+* Please be aware that a policy redeploy is needed to update the Group Objects in the used Policies. Currently there is an optional API call built in to do a policy redeploy, however please take caution in using this, since this might cause other, unrelated policies or objects to be deployed (e.g., if another network administrator is working on a Policy in the GUI).
+
+* Important is to use SSL verification and to test the script before running this in a production environment. In the config.json file, set the *"SSL_VERIFY"* parameter to *true*, and then set *"SSL_CERT"* to be the path to the FMC's certificate.
+
+* Please test this properly before implementing in a production environment. This is a sample script.
+
+* In case the intervalScheduler is used: the running script should be hosted in a secure environment! For example: if a malicious actor can place additional IP-addresses or URL's in the list somehow, they will be put in a Firepower trust rule, and might cause the malicious actor to bypass security.
+
+
+## Author(s)
+
+* Christopher van der Made (Cisco)
+* Alan Nix (Cisco)
+
